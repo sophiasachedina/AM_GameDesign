610e913d733762190079a409838f52259fb683ab
sophiasachedina <sachedinas24@greenhill.org>
2022-06-01 10:10:45 -0500

    sophia

---
+++ TwoNumber.py
@@ -0,0 +1,35 @@
+#Maria I Suarez
+
+#Two numbers
+
+# We will ask user for two number
+
+# and add them
+
+#  Any thing after # is
+
+# ignored by the computer
+
+#import things
+
+import os
+
+os.system('cls')
+
+#Use Variables
+
+Num1=23
+
+Num2=12
+
+answer=Num1+Num2
+
+#Operators +,-, *,/
+
+print('The addition of ',Num1,'and ', Num2, 'is ', answer)
+
+print(Num1) #after each print it makes a return
+
+print(Num2, end='/ ') #change end to space
+
+print(answer)
